415f37d3ffa9a129cd84dec3edd1e4b16b5f78c8
Danil Semelenov <mail@danil.mobi>
2018-11-21 04:46:18 +0300

    Set a more precise initial height for iframe

--- scripts/youcanbookme.js
+++ scripts/youcanbookme.js
@@ -1,4 +1,5 @@
 export default element => {
+  element.style.height = '593px';
   addEventListener('message', event => {
     if (event.origin === 'https://uptocoaching.youcanbook.me') {
       element.style.height = `${event.data - 100}px`;
