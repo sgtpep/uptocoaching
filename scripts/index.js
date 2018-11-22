9b7878df20d69c4575cdbd638caf70e13d0b0797
Danil Semelenov <mail@danil.mobi>
2018-11-22 23:00:18 +0300

    Extract variables, check if the booking element is present

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,15 @@ import pageJumps from './page-jumps.js';
 import youcanbookme from './youcanbookme.js';
 
 encodedData();
+
+const header = document.querySelector('.header');
 navigationActivation(
   document.querySelectorAll('.header-navigation a[href*="#"]'),
-  document.querySelector('.header'),
+  header,
 );
-pageJumps(document.querySelector('.header'));
-youcanbookme(document.querySelector('.book > iframe'));
+pageJumps(header);
+
+const booking = document.querySelector('.book > iframe');
+if (booking) {
+  youcanbookme(booking);
+}
