6702a8ea44b31bc91190d915c3382a8d2885559c
Danil Semelenov <mail@danil.mobi>
2018-11-26 00:47:09 +0300

    Implement the parallax animation, adjust styles

--- scripts/animate-parallaxes.js
+++ scripts/animate-parallaxes.js
@@ -1,3 +1,29 @@
+const onScroll = elements => {
+  const { top } = document.body.getBoundingClientRect();
+  elements.forEach(element =>
+    ['msTransform', 'transform', 'webkitTransform'].forEach(property => {
+      element.style[property] = `translate3d(0, ${(
+        Math.min(
+          Math.max(
+            (pageYOffset -
+              (element.parentElement.getBoundingClientRect().top - top) +
+              (innerHeight - element.parentElement.offsetHeight) / 2) /
+              -(innerHeight + element.parentElement.offsetHeight),
+            -0.5,
+          ),
+          0.5,
+        ) * element.parentElement.offsetHeight
+      ).toFixed(1)}px, 0)`;
+    }),
+  );
+};
+
 export default elements => {
-  [...elements].forEach(element => {});
+  [...elements].forEach(element =>
+    element.parentElement.classList.add('parallax-container'),
+  );
+  addEventListener('scroll', () =>
+    requestAnimationFrame(() => onScroll(elements)),
+  );
+  onScroll(elements);
 };
