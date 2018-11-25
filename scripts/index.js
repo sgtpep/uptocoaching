0adb0db8ea6be0af9b60ddcb845d1ffd933376e5
Danil Semelenov <mail@danil.mobi>
2018-11-25 15:42:58 +0300

    Rename scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 import activateNavigation from './activate-navigation.js';
 import decodeProperties from './decode-properties.js';
-import pageJumps from './page-jumps.js';
-import youcanbookMe from './youcanbook-me.js';
+import offsetPageJumps from './offset-page-jumps.js';
+import resizeYoucanbookMe from './resize-youcanbook-me.js';
 
 const header = document.querySelector('.header');
 activateNavigation(
   header,
   document.querySelectorAll('.header-navigation a[href*="#"]'),
 );
-pageJumps(header);
+offsetPageJumps(header);
 
 decodeProperties();
 
-const booking = document.querySelector('.book > iframe');
-if (booking) {
-  youcanbookMe(booking);
+const youcanbookMe = document.querySelector('.book > iframe');
+if (youcanbookMe) {
+  resizeYoucanbookMe(youcanbookMe);
 }
