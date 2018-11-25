50f4ef8a0d2de74a496c2c2e28fa48529e517baa
Danil Semelenov <mail@danil.mobi>
2018-11-25 15:45:53 +0300

    Add a script stub

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 import activateNavigation from './activate-navigation.js';
+import animateParallaxes from './animate-parallaxes.js';
 import decodeProperties from './decode-properties.js';
 import offsetPageJumps from './offset-page-jumps.js';
 import resizeYoucanbookMe from './resize-youcanbook-me.js';
@@ -10,6 +11,7 @@ activateNavigation(
 );
 offsetPageJumps(header);
 
+animateParallaxes(document.querySelectorAll('.parallax'));
 decodeProperties();
 
 const youcanbookMe = document.querySelector('.book > iframe');
