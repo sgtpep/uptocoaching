3956b7b15601911d970a1e03e7e370a4a3b4e85e
Danil Semelenov <mail@danil.mobi>
2018-12-07 21:19:38 +0300

    Rename a script

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 import activateNavigation from './activate-navigation.js';
 import decodeProperties from './decode-properties.js';
-import offsetPageJumps from './offset-page-jumps.js';
+import pageJumps from './page-jumps.js';
 import parallaxScrolling from './parallax-scrolling.js';
 import youcanbookMe from './youcanbook-me.js';
 
@@ -9,7 +9,7 @@ activateNavigation(
   header,
   document.querySelectorAll('.header-navigation a[href*="#"]'),
 );
-offsetPageJumps(header);
+pageJumps(header);
 
 decodeProperties();
 parallaxScrolling(document.querySelectorAll('.parallax'));
