43a9d7793ae56f5787224b894957821efed0b7df
Danil Semelenov <mail@danil.mobi>
2018-12-07 21:43:52 +0300

    Replace a script with a[name]

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,12 @@
 import activateNavigation from './activate-navigation.js';
 import decodeProperties from './decode-properties.js';
-import pageJumps from './page-jumps.js';
 import parallaxScrolling from './parallax-scrolling.js';
 import youcanbookMe from './youcanbook-me.js';
 
-const header = document.querySelector('.header');
 activateNavigation(
-  header,
+  document.querySelector('.header'),
   document.querySelectorAll('.header-navigation a[href*="#"]'),
 );
-pageJumps(header);
-
 decodeProperties();
 parallaxScrolling(document.querySelectorAll('.parallax'));
 
