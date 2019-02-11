ce8a3cac4afd61732f40ecbc22656335efe12291
Danil Semelenov <mail@danil.mobi>
2019-02-11 23:20:56 +0300

    Replace js-based smooth scrolling with css

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,9 @@
 import activateNavigation from './activate-navigation.js';
 import decodeProperties from './decode-properties.js';
 import parallaxScrolling from './parallax-scrolling.js';
-import smoothPageJumps from './smooth-page-jumps.js';
 import youcanbookMe from './youcanbook-me.js';
 
-const anchors = document.querySelectorAll('.header-navigation a[href*="#"]');
-activateNavigation(anchors);
-smoothPageJumps(anchors);
-
+activateNavigation(document.querySelectorAll('.header-navigation a[href*="#"]'));
 decodeProperties();
 parallaxScrolling(document.querySelectorAll('.parallax'));
 
